409a063d95460b8dfd57c220ee864729714b6aab
Dylan Ellis <dylan00ellis@gmail.com>
2022-05-16 11:11:33 +0200

    added comments

--- APIs.js
+++ APIs.js
@@ -1,71 +1,68 @@
-const TwitWebApi = require('twit');
-const SpotifyWebApi = require('spotify-web-api-node');
-const OMdbWebApi = new (require('omdbapi'))('dfea9817');
-const fs = require('fs');
+const TwitWebApi = require('twit');  // define the Twitter API
+const SpotifyWebApi = require('spotify-web-api-node');  // define the Spotify API
+const OMdbWebApi = new (require('omdbapi'))('dfea9817');  // define the OMdb Api and create an object for it
+const fs = require('fs');  // define the object that will be used to handel file input and output
 
-const T = new TwitWebApi({
+const T = new TwitWebApi({  // create twitter object
     consumer_key: 'kbB99piXAAou57ht6Gv9XsGRc',
   consumer_secret: 'tH6OiDkI9t9BI0s5LN22w5ecPURyVdWT2RKSLK4S542PYHamCV-LE6uR6P3UKOwUecs-4WA',
   access_token: '1524380101433044992-Yr4SEg12D8b8zFaLfjOncP8SJuAgur',
   access_token_secret: 'pJQvj3P9JdAbZvYOljksLbumERaeWsbysJWQw70MBjI9n'
 });
 
-const spotifyApi = new SpotifyWebApi();
+const spotifyApi = new SpotifyWebApi(); // create spotify object
 
-module.exports = {
-    getTweets: function(userName){      
-        Name = userName;
-        
+module.exports = {  // export all the functions at once
+    getTweets: function(userName){  // difine a function to get the tweets
         let params = {query: userName, count: 20} 
         
-        T.get('search/tweets', params,searchedData); 
-        function searchedData(error, data) {
-            if (error) {
-                console.log('An error occurred while trying to search for tweets: ' + error);
+        T.get('search/tweets', params, (error, data) => {  // call the get method on the twitter object to get the tweet
+            if (error) {  // handel the error if there is one
+                console.log('An error occurred while trying to search for tweets: ' + error);  // display the error message
             } 
-            else {
+            else {  // return the tweets
                 return data.body;
             }
-        }
+        });  
     },
 
-    getSongInfo: function(songName){
-        spotifyApi.searchTracks(songName)
+    getSongInfo: function(songName){  // define the function to get song information
+        spotifyApi.searchTracks(songName)  // call the searchTracks function to get the information
             .then(data => {
-                return data.body;
+                return data.body;  // return the information
             }, 
-            function(error) {
-                console.log('An error occurred while trying to search for the song: ' + error);
+            function(error) {  // handle errors that might be thrown
+                console.log('An error occurred while trying to search for the song: ' + error);  // display the error message
         });
     },
 
-    getMovieInfo: function(movieName){
-        OMdbWebApi.get({title: movieName})
+    getMovieInfo: function(movieName){  // define the function to get movie information
+        OMdbWebApi.get({title: movieName})  // call the get method on the omdb object to get the information
             .then(data => {
-                return data.body;
+                return data.body;  // return the information
             }, 
-            function(error) {
-                console.log('An error occurred while trying to search for the movie: ' + error);
+            function(error) {  // handle errors that might be thrown
+                console.log('An error occurred while trying to search for the movie: ' + error);  // display the error message
         });
     },
 
-    fileInfo: function(){
-        let arrData = []; 
+    fileInfo: function(){  // define the function to execute a query from a text file
+        let arrData = [];  // create array that will store the query and search string
 
-        try { 
-            let data = fs.readFileSync('random.text');
-            let strData = data.toString();
+        try {  // handle errors that might be thrown
+            let data = fs.readFileSync('random.text');  // read the data from the file
+            let strData = data.toString();  // convert the data to a string
 
-            arrData = strData.split(','); 
+            arrData = strData.split(',');  // split the string into the query and search string
             
-            if (arrData[0] === 'Song name'){
-                return getSongInfo(arrData[1]);
+            if (arrData[0] === 'Song name'){  // check if the query is for a song
+                return getSongInfo(arrData[1]);  // return the song information
             }
-            else if(arrData[0] === 'Movie name'){
-                return getMovieInfo(arrData[1]);
+            else if(arrData[0] === 'User name'){  // check if the query is for a twitter user
+                return this.getTweets(arrData[1]);  // return the user tweets
             }
             else{
-                return 'Invalid query';
+                return 'Invalid query';  // display error message is query is invalid
             }
 
         } catch(error) {
--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-const readline = require("readline-sync");
+const readline = require("readline-sync");  // create an object for readline to be able to get user input.
 
-const api = require('./APIs');
+const api = require('./APIs');  // import the local module that contains the code to make API calls
 
-let choice;
+let choice;  // define a variable that will store the users choice
 
-while (choice !== '5') {
+while (choice !== '5') {  // keep on showing the menu to the user until they choose to exit
+    // display the menu options
     console.log();
     console.log('----------------------------------------------------------------');
     console.log('(1) Print latest tweets');
@@ -14,32 +15,32 @@ while (choice !== '5') {
     console.log('(5) Exit');
     console.log('----------------------------------------------------------------\n');
 
-    choice = readline.question("Pick an option: \n");
+    choice = readline.question("Pick an option: \n");  // read the user input into the choice variable
 
-    switch(choice) {
+    switch(choice) {  // switch on the users choice to perform the option they selected
         case '1':
-            try {
-                userName = readline.question("Enter the username: \n");
-                data = api.getTweets(userName);
+            try {  // error handeling to make sure the application doesn't crash when an error is encountered
+                userName = readline.question("Enter the username: \n");  // ask for the username
+                data = api.getTweets(userName);  // get the tweets for the specified username
 
-                let tweetData = JSON.parse(data);
-                console.log('Tweets: ' + tweetData.tweets);
+                let tweetData = JSON.parse(data);  // store the tweet data in an object that can be used to retrieve only the tweets
+                console.log('Tweets: ' + tweetData.tweets); // display the tweet data
             } catch (error) {
                 console.log('An unexpected error occurred see below: \n' + error.message);
             }
         break;
 
         case '2':
-            try {
-                song = readline.question("Enter the song name: \n");
-                data = api.getSongInfo(song);
+            try {  // error handeling to make sure the application doesn't crash when an error is encountered
+                song = readline.question("Enter the song name: \n");  // ask for the song name
+                data = api.getSongInfo(song);  // get the info for the specified song
 
-                let songInfo = JSON.parse(data);
-                console.log('Artist: ' + songInfo.artist);
-                console.log('Song: ' + songInfo.name);
-                console.log('Preview: ' + songInfo.preview_url);
-                console.log('Album: ' + songInfo.album);
-            } catch (error) {
+                let songInfo = JSON.parse(data);  // store the song information in an object that can be used to retrieve the needed information
+                console.log('Artist: ' + songInfo.artist);  // display the artist information
+                console.log('Song: ' + songInfo.name);  // display the song information
+                console.log('Preview: ' + songInfo.preview_url);  // display the preview link
+                console.log('Album: ' + songInfo.album);  // display the album information
+            } catch (error) { 
                 console.log('An unexpected error occurred see below: \n' + error.message);
             }
         break;
@@ -57,18 +58,18 @@ while (choice !== '5') {
         break;
 
         case '4':
-            try {
-                console.log(api.fileInfo());
+            try {  // error handeling to make sure the application doesn't crash when an error is encountered
+                console.log(api.fileInfo());  // display the information returned from running the query in the text file
             } catch (error) {
                 console.log('An unexpected error occurred see below: \n' + error.message);
             }
         break;
 
         case '5':
-            console.log('Goodbye come back soon.\n');
+            console.log('Goodbye come back soon.\n');  // display a goodbye message
         break;
 
-        default:
-            console.log('Invalid option entered, please enter a valid option in the menu (1-4)\n');
+        default:  // handel any invalid input from the user
+            console.log('Invalid option entered, please enter a valid option in the menu (1-5)\n');  // display a message to say an incorrect option was chosen
       }
 }
